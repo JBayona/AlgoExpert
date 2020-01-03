251be35151ed110b55420a1fefdd236a1f2e4161
Jorge Bayona <jbayonaromo@lyft.com>
2020-01-03 15:40:41 -0800

    getNthFib constant space.

--- easy/getNthFib.js
+++ easy/getNthFib.js
@@ -46,6 +46,19 @@ function getNthFib(n) {
     return hash[n];
 }
 
-console.log(getNthFib(6));
+// O(n) time | O(1) space
+
+function getNthFib(n) {
+    const lastTwo = [0, 1];
+    let counter = 3;
+    while(counter <= n) {
+        const nextFib = lastTwo[0] + lastTwo[1];
+        lastTwo[0] = lastTwo[1];
+        lastTwo[1] = nextFib;
+        counter++;
+    }
+
+    return n > 1 ? lastTwo[1] : lastTwo[0];
+}
 
 console.log(getNthFib(6));
