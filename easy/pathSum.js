7a54d62bb93461cb4b32b9f8966dff3876f8614c
JBayona <jordavids_22@hotmail.com>
2021-04-14 21:29:20 -0500

    Path Sum.

--- easy/pathSum.js
+++ easy/pathSum.js
@@ -1,6 +1,7 @@
 /*
 Given the root of a binary tree and an integer targetSum, return true
-if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+if the tree has a root-to-leaf path such that adding up all the values along the
+path equals targetSum.
 
 A leaf is a node with no children.
 
